564322caf595f015eb769cd1eb9bbb0845bf8bca
William Barbosa <william.sb@hotmail.com>
2016-04-12 16:50:16 -0300

    Working Droid bindings

---
+++ Layer.Droid/Additions/IComparableErrors.cs
@@ -0,0 +1,164 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Java.Lang;
+
+// ReSharper disable once CheckNamespace
+namespace Com.Layer.Transport.Thrift.Identity
+{
+    public partial class Error
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Follow
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class FollowsRequest
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Identity
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class IdentityChangedBatch
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class IdentityPatch
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Response
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class UpdateIdentity
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class IdentityUpdatesResponse
+    {
+        public int CompareTo(Object another) => 0;
+    }
+}
+
+namespace Com.Layer.Transport.Thrift.Policy
+{
+    public partial class Error
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Response
+    {
+        public int CompareTo(Object another) => 0;
+    }
+}
+
+namespace Com.Layer.Transport.Thrift.Sync
+{
+    public partial class Error
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Content
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class CompoundStream
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Event
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class PartialSyncHints
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class PushToken
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Response
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class Stream
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class StreamMetadata
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class SyncUserMutation
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class SyncUserMutations
+    {
+        public int CompareTo(Object another) => 0;
+    }
+}
+
+namespace Com.Layer.Transport.Thrift.Scrlk.Badging
+{
+    public partial class BadgeRequest
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class PartialSyncHints
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    public partial class PartialSyncHintsRequest
+    {
+        public int CompareTo(Object another) => 0;
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public partial class Badging
+    {
+        public partial class Get_badge_counts_args
+        {
+            public int CompareTo(Object another) => 0;
+        }
+
+        public partial class Get_badge_counts_result
+        {
+            public int CompareTo(Object another) => 0;
+        }
+
+        public partial class Get_partial_sync_hints_args
+        {
+            public int CompareTo(Object another) => 0;
+        }
+
+        public partial class Get_partial_sync_hints_result
+        {
+            public int CompareTo(Object another) => 0;
+        }
+    }
+}
